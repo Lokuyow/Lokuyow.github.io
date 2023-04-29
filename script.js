942ae85de1fa2068611f7c0aedb116775b4eadab
Lokuyow <cg.magnet@gmail.com>
2023-04-29 10:34:36 +0900

    add gazo

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 document.addEventListener("DOMContentLoaded", function () {
     const images = [
-      { src: "icon/hutu.png", probability: 30 },
-      { src: "icon/huhe.png", probability: 25 },
-      { src: "icon/nita.png", probability: 20 },
+      { src: "icon/hutu.png", probability: 15 },
+      { src: "icon/huhe.png", probability: 15 },
+      { src: "icon/nita.png", probability: 10 },
+      { src: "icon/maji.png", probability: 23 },
       { src: "icon/bero.png", probability: 7 },
+      { src: "icon/basu.png", probability: 7 },
       { src: "icon/kowa.png", probability: 5 },
       { src: "icon/kowa2.png", probability: 5 },
       { src: "icon/nita0.png", probability: 5 },
-      { src: "icon/tiku.png", probability: 2 },
-      { src: "icon/tiku2.png", probability: 1 },
+      { src: "icon/ike.png", probability: 3 },
+      { src: "icon/tiku.png", probability: 3 },
+      { src: "icon/tiku2.png", probability: 2 },
     ];
   
     const randomImage = document.getElementById("randomImage");
