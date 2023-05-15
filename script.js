0c7b3adac0ea27c412c4853d45dd98e74d1425d7
Lokuyow <cg.magnet@gmail.com>
2023-05-15 16:20:26 +0900

    ramen tsukemen

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 document.addEventListener("DOMContentLoaded", function () {
     const images = [
-      { src: "icon/hutu.png", probability: 16 },
+      { src: "icon/hutu.png", probability: 14 },
       { src: "icon/huhe.png", probability: 10 },
-      { src: "icon/nita.png", probability: 15 },
+      { src: "icon/nita.png", probability: 14 },
       { src: "icon/maji.png", probability: 12 },
       { src: "icon/bero.png", probability: 7 },
       { src: "icon/basu.png", probability: 7 },
       { src: "icon/kowa.png", probability: 5 },
       { src: "icon/kowa2.png", probability: 5 },
       { src: "icon/nita0.png", probability: 5 },
-      { src: "icon/ike.png", probability: 2 },
+      { src: "icon/ike2.png", probability: 5 },
       { src: "icon/tiku.png", probability: 2 },
       { src: "icon/tiku2.png", probability: 2 },
       { src: "icon/note13kmrhvkpnqk3tkg4z4x7527aqejqg90vk8hwe38khmd9hn29lcwsr5qxaj.jpg", probability: 2 },
