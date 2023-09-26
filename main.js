189b210a004d0366bbdcc0a28af652072bfbdbaf
Lokuyow <cg.magnet@gmail.com>
2023-09-26 17:14:03 +0900

    add: made

--- main.js
+++ main.js
@@ -48,11 +48,3 @@ document.addEventListener('DOMContentLoaded', async () => {
     }
   });
 });
-
-function setVH() {
-  let vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
-}
-
-window.addEventListener('resize', setVH);
-window.addEventListener('load', setVH);
